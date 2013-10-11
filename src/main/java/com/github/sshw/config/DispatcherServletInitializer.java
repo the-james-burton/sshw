825a623a0a13044d0765c6f63dcc5007ddcf9a7e
the-james-burton <the.james.burton@gmail.com>
2013-10-11 16:27:04 +0100

    corrected package name

--- src/main/java/com/github/sshw/config/DispatcherServletInitializer.java
+++ src/main/java/com/github/sshw/config/DispatcherServletInitializer.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.github.sshw.config;
+package com.github.sshw.config;
 
 import javax.servlet.ServletRegistration.Dynamic;
 
--- src/main/java/com/github/sshw/config/WebConfig.java
+++ src/main/java/com/github/sshw/config/WebConfig.java
@@ -1,4 +1,4 @@
-package org.github.sshw.config;
+package com.github.sshw.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
